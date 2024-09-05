43c76219d33b24b1c957113613cf5e145dc6f5d0
Trent Cowden <trent.cowden@gmail.com>
2024-09-05 12:13:58 -0700

    metadata/favicon

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app'
 import RootLayout from './layout'
 
-export default function MyApp({ Component, pageProps, router }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
       <Component {...pageProps} />
