0015f0db06853863b9341a451aaf2f6dc7a59cc7
Trent Cowden <trent.cowden@gmail.com>
2024-09-14 20:27:15 -0600

    fixing key error

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -13,26 +13,26 @@ export default function HowItWorks() {
         [
           'Upload Your Files',
           'Upload your audio files and matching text transcript files. Make sure their file names match to ensure correct pairing.',
-          <TbCircleNumber1 className="size-8 text-p1 mb-2" />,
+          <TbCircleNumber1 key="1" className="size-8 text-p1 mb-2" />,
           // 'https://firebasestorage.googleapis.com/v0/b/waha-ai-timestamper-4265a.appspot.com/o/images%2Fezgif-7-85516498a6.gif?alt=media',
         ],
         [
           'Detect Language',
           'Timestamp Audio will automatically detect the language of your audio files, or you can select manually from 1100+ languages  .',
-          <TbCircleNumber2 className="size-8 text-p1 mb-2" />,
+          <TbCircleNumber2 key="2" className="size-8 text-p1 mb-2" />,
         ],
         [
           'Timestamp!',
           'Using AI, the app will timestamp the text based on its corresponding section in the audio.',
-          <TbCircleNumber3 className="size-8 text-p1 mb-2" />,
+          <TbCircleNumber3 key="3" className="size-8 text-p1 mb-2" />,
           // 'https://firebasestorage.googleapis.com/v0/b/waha-ai-timestamper-4265a.appspot.com/o/images%2Fezgif-4-74833150a6.gif?alt=media',
         ],
         [
           'Download your data',
           'Download your timestamped data as json and add to your web app or as srt to easily add subtitles to your videos.',
-          <TbCircleNumber4 className="size-8 text-p1 mb-2" />,
+          <TbCircleNumber4 key="4" className="size-8 text-p1 mb-2" />,
         ],
-      ].map(([title, description, icon], index) => (
+      ].map(([title, description, icon]) => (
         <div
           key={title as string}
           className="card bg-b2 px-6 py-6 overflow-hidden mb-2"
